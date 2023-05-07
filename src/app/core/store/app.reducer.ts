829385dcd8bfe702961b27cb106ce06a1a5471fa
Nabendu Manna <69715068+Nabendu-Manna@users.noreply.github.com>
2023-05-07 11:21:34 +0530

    init

--- src/app/core/store/app.reducer.ts
+++
@@ -1,6 +0,0 @@
-import { sharedReducer } from "src/app/shared/store/shared/shared.reducer";
-import { SHARED_STATE_NAME } from "src/app/shared/store/shared/shared.selector";
-
-export const appReducer = {
-  [SHARED_STATE_NAME]: sharedReducer,
-}
--- src/app/core/store/app.state.ts
+++
@@ -1,6 +0,0 @@
-import { SHARED_STATE_NAME } from "src/app/shared/store/shared/shared.selector";
-import { SharedState } from "src/app/shared/store/shared/shared.state";
-
-export interface AppState {
-    [SHARED_STATE_NAME]: SharedState;
-}
